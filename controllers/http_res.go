9264fda6c4d6e1b0581aa8f5fe933188e2e50579
setthasit <setthasit.th@gmail.com>
2022-03-24 23:24:42 +0700

    fix error response argument type

--- controllers/http_res.go
+++ controllers/http_res.go
@@ -2,10 +2,10 @@ package controllers
 
 import "github.com/gin-gonic/gin"
 
-func responsMessageHttp(c *gin.Context, status int, message interface{}) {
+func responsMessageHttp(c *gin.Context, status int, message string) {
 	type baseResponse struct {
-		StatusCode int         `json:"status_code"`
-		Message    interface{} `json:"message"`
+		StatusCode int    `json:"status_code"`
+		Message    string `json:"message"`
 	}
 
 	c.JSON(status, &baseResponse{
