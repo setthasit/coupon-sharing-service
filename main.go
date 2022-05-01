b18ffd5bdc51892a87ffe8ae0ed3c63f972bc3ea
art.t <setthasit.th@gmail.com>
2022-05-01 12:26:53 +0700

    graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
 	"coupon-service/di"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -9,5 +16,28 @@ func main() {
 
 	app.Ctrl.RegisterRoute(app.Engine)
 
-	app.Engine.Run(":9999")
+	srv := &http.Server{
+		Addr:    ":9999",
+		Handler: app.Engine,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf(err.Error())
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server force to shutdown!: %q", err)
+	}
+
+	log.Println("Server gracefully shut down")
 }
